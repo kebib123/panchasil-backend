d744e67864741ad251cbdf522c157682cb293dcf
ashish336b <ashish336b@gmail.com>
2020-09-15 19:31:29 +0545

    db seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,11 +1,9 @@
 <?php
 
-use Carbon\Traits\Date;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
-use Faker\Factory as Faker;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -16,35 +14,35 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::table("users")->insert([
-            "name"=>"admin",
-            "email"=>"admin@admin.com",
-            "password"=> Hash::make("11111111")
+            "name" => "admin",
+            "email" => "panchasilmedia@gmail.com",
+            "password" => Hash::make("@@panchasil@@"),
         ]);
         /* DB::table("users")->insert([
-            "name"=>"ashish",
-            "email"=>"ashish336b@gmail.com",
-            "password"=> Hash::make("11111111")
+        "name"=>"ashish",
+        "email"=>"ashish336b@gmail.com",
+        "password"=> Hash::make("11111111")
         ]); */
         /* DB::table("categories")->insert([
-            "name"=>"Testing",
-            "status"=>"pending",
-            "image"=>"a.jpg",
-            "slug"=>"testing"
-        ]);
-        $faker = Faker::create('App/News');
-        for($i=0;$i<100;$i++){
-            DB::table("news")->insert([
-                "title"=>$faker->firstName,
-                "author"=>$faker->lastName,
-                "image"=>"a.jpg",
-                "taja_khabar"=>null,
-                "headline"=>null,
-                "related"=>null,
-                "description"=>"asdf",
-                "category_id"=>1,
-                "status"=>"pending",
-                "slug"=>"news$i",
-            ]);
-        } */
+    "name"=>"Testing",
+    "status"=>"pending",
+    "image"=>"a.jpg",
+    "slug"=>"testing"
+    ]);
+    $faker = Faker::create('App/News');
+    for($i=0;$i<100;$i++){
+    DB::table("news")->insert([
+    "title"=>$faker->firstName,
+    "author"=>$faker->lastName,
+    "image"=>"a.jpg",
+    "taja_khabar"=>null,
+    "headline"=>null,
+    "related"=>null,
+    "description"=>"asdf",
+    "category_id"=>1,
+    "status"=>"pending",
+    "slug"=>"news$i",
+    ]);
+    } */
     }
 }
